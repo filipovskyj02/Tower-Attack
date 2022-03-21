e35d557fb0b0bed4de2d8f555c83f99a87ee0393
Jakub Filipovský <filipj25@fit.cvut.cz>
2022-03-21 23:53:38 +0100

    i have had enough

--- hw02/main.cpp
+++ hw02/main.cpp
@@ -24,20 +24,30 @@ class CVATRegister
         string c_ID;
         string c_NAME;
         string c_ADDRESS;
-        int c_Balance = 0;
+        unsigned int c_Balance = 0;
 
     };
 public:
     vector<Company*> pointerVecId;
     vector<Company*> pointerVecNameAddr;
+    vector<unsigned int>transactions;
+    unsigned int pos = 0;
 
     void print (){
         for (int i = 0; i < pointerVecId.size(); i++){
 
-            cout << "Name : " << pointerVecId[i]->c_Name << " Address: " << pointerVecId[i]->c_Address << " ID: " << pointerVecId[i]->c_ID << endl;
+            cout << "Name : " << pointerVecId[i]->c_Name << " Address: " << pointerVecId[i]->c_Address << " ID: " << pointerVecId[i]->c_ID << "Balance :" << pointerVecId[i]->c_Balance << endl;
 
 
         }
+        printf("------------------------------------------------------------------------\n");
+        for (int i = 0; i < pointerVecNameAddr.size(); i++){
+
+            cout << "Name : " << pointerVecNameAddr[i]->c_Name << " Address: " << pointerVecNameAddr[i]->c_Address << " ID: " << pointerVecNameAddr[i]->c_ID << " Balance: " << pointerVecNameAddr[i]->c_Balance<<endl;
+
+
+        }
+
 
 
 
@@ -48,22 +58,58 @@ public:
 
         return a->c_ID < b ? true : false;
     }
+
     bool  static cmpName(const Company * a,const string & b) {
 
-        return a->c_Name < b ? true : false;
+        return a->c_NAME < b ? true : false;
     }
 
 
     bool  static cmpAdr(const Company * a,const string & b) {
 
-        return a->c_Address < b ? true : false;
+        return a->c_ADDRESS < b ? true : false;
+    }
+    auto findIndex (const string    & taxID)const{
+        auto pos = lower_bound(pointerVecId.begin(),pointerVecId.end(), taxID, cmpID);
+        if (pos == pointerVecId.end()) return pointerVecId.end();
+        if ((*pos)->c_ID != taxID) return pointerVecId.end();
+
+        return pos;
+
+
+
+
+    }
+    auto findIndex (const string    & name,
+                    const string    & addr)const{
+        string NAME = name;
+        string ADDR = addr;
+
+        transform(NAME.begin(), NAME.end(), NAME.begin(),::toupper);
+        transform(ADDR.begin(), ADDR.end(), ADDR.begin(),::toupper);
+
+        auto pos = lower_bound(pointerVecNameAddr.begin(),pointerVecNameAddr.end(), NAME, cmpName);
+        if (pos == pointerVecNameAddr.end())return pointerVecNameAddr.end();
+        if ((*pos)->c_NAME != NAME) return pointerVecNameAddr.end();
+
+        auto pos1 = lower_bound(pos,pointerVecNameAddr.end(), ADDR, cmpAdr);
+        if (pos1 == pointerVecNameAddr.end())return pointerVecNameAddr.end();
+        if ((*pos1)->c_NAME != NAME or (*pos1)->c_ADDRESS != ADDR) return pointerVecNameAddr.end();
+
+        return pos1;
+
+
+
+
+
+
+    }
+    void add (void ){
+        pos++;
     }
 
 
 
-
-                  CVATRegister   ( void );
-                  ~CVATRegister  ( void );
 
 
     bool          newCompany     ( const string    & name,
@@ -81,16 +127,25 @@ public:
         auto posName = pointerVecNameAddr.begin();
         if (!pointerVecId.empty()) {
             posID = lower_bound(pointerVecId.begin(), pointerVecId.end(), taxID, cmpID);
-            if ((*posID)->c_ID == taxID) return false;
+            if (posID != pointerVecId.end()) {
+                if ((*posID)->c_ID == taxID) return false;
+            }
 
-            posName = lower_bound(pointerVecNameAddr.begin(),pointerVecNameAddr.end(),name,cmpName);
-            if ((*posName)->c_Name == name){
+            posName = lower_bound(pointerVecNameAddr.begin(),pointerVecNameAddr.end(),NAME,cmpName);
+            if (posName != pointerVecNameAddr.end()){
+            if ((*posName)->c_NAME == NAME){
                 auto end = posName;
-                while ((*posName)->c_Name == name){end++;}
+                while ((*end)->c_NAME == NAME){
+                    end++;
+                    if (end == pointerVecNameAddr.end()) break;
+                }
                 auto tmp = posName;
-                posName = lower_bound(tmp,end,addr,cmpAdr);
+                posName = lower_bound(tmp,end,ADDR,cmpAdr);
+                if (posName != pointerVecNameAddr.end()) {
+                    if ((*posName)->c_ADDRESS == ADDR) return false;
+                }
 
-                if ((*posName)->c_ADDRESS == addr) return false;
+            }
 
             }
 
@@ -99,8 +154,8 @@ public:
             addNew->c_ID.append(taxID);
             addNew->c_Name.append(name);
             addNew->c_Address.append(addr);
-            addNew->c_NAME = NAME;
-            addNew->c_ADDRESS = ADDR;
+            addNew->c_NAME.append(NAME);
+            addNew->c_ADDRESS.append(ADDR);
             pointerVecId.insert(posID, addNew);
             pointerVecNameAddr.insert(posName, addNew);
 
@@ -114,25 +169,122 @@ public:
 
 
         bool          cancelCompany  ( const string    & name,
-                                   const string    & addr );
+                                   const string    & addr ){
+            auto pos = findIndex(name,addr);
+            if (pos == pointerVecNameAddr.end()) return false;
+            pointerVecNameAddr.erase(pos);
+
+            auto pos1 = findIndex((*pos)->c_ID);
+            pointerVecId.erase(pos1);
+
+            delete (*pos1);
+            return true;
+
+
+
+    }
     bool          cancelCompany  ( const string    & taxID ){
+        auto pos = findIndex(taxID);
+        if (pos == pointerVecId.end()) return false;
+
+        pointerVecId.erase(pos);
+
+        auto pos1 = findIndex((*pos)->c_Name, (*pos)->c_Address);
+        pointerVecNameAddr.erase(pos1);
+
+        delete (*pos1);
+        return true;
+
 
     }
     bool          invoice        ( const string    & taxID,
-                                   unsigned int      amount );
+                                   unsigned int      amount ){
+        auto pos = findIndex(taxID);
+        if (pos == pointerVecId.end()) return false;
+
+        (*pos)->c_Balance += amount;
+        transactions.push_back(amount);
+        return true;
+
+    }
+
     bool          invoice        ( const string    & name,
                                    const string    & addr,
-                                   unsigned int      amount );
+                                   unsigned int      amount ){
+       auto pos = findIndex(name,addr);
+        if (pos == pointerVecNameAddr.end()) return false;
+        (*pos)->c_Balance += amount;
+        transactions.push_back(amount);
+        return true;
+
+
+    }
     bool          audit          ( const string    & name,
                                    const string    & addr,
-                                   unsigned int    & sumIncome ) const;
+                                   unsigned int    & sumIncome ) const{
+
+        auto pos = findIndex(name,addr);
+        if (pos == pointerVecNameAddr.end()) return false;
+
+        sumIncome = (*pos)->c_Balance;
+        return true;
+
+
+    }
+
     bool          audit          ( const string    & taxID,
-                                   unsigned int    & sumIncome ) const;
+                                   unsigned int    & sumIncome ) const{
+        auto pos = findIndex(taxID);
+        if (pos == pointerVecId.end()) return false;
+
+        sumIncome = (*pos)->c_Balance;
+        return true;
+
+
+
+    }
     bool          firstCompany   ( string          & name,
-                                   string          & addr ) const;
+                                   string          & addr ) const{
+        if (pointerVecNameAddr.empty()) return false;
+            name = pointerVecNameAddr[0]->c_Name;
+            addr = pointerVecNameAddr[0]->c_Address;
+
+            return true;
+
+
+
+
+
+
+    }
     bool          nextCompany    ( string          & name,
-                                   string          & addr ) const;
-    unsigned int  medianInvoice  ( void ) const;
+                                   string          & addr ) {
+        add();
+        if (pos >= pointerVecNameAddr.size()) return false;
+
+        name = pointerVecNameAddr[pos]->c_Name;
+        addr = pointerVecNameAddr[pos]->c_Address;
+        return true;
+
+
+
+    }
+   unsigned int  medianInvoice  ( void ) const{
+       if (transactions.empty()) return 0;
+    vector<unsigned int> trCopy;
+        for (auto i = 0; i < transactions.size(); i++){
+            trCopy.push_back(transactions.at(i));
+
+        }
+    sort(trCopy.begin(),trCopy.end());
+    int pos = trCopy.size()/2;
+
+
+        return trCopy.at(pos);
+
+
+    }
+
   private:
 
 
@@ -142,47 +294,53 @@ public:
 
 
 
-
 //#ifndef __PROGTEST__
 int               main           ( void )
 {
   string name, addr;
   unsigned int sumIncome;
+  /*
     //cout << ("666/666" < "123456") << endl;
   CVATRegister b1;
-    assert ( b1 . newCompany ( "ACME1", "Kolejni1", "666/666/666/666" ) );
+
     assert ( b1 . newCompany ( "Dummy", "Thakurova", "123456" ) );
   assert ( b1 . newCompany ( "ACME", "Thakurova", "666/666" ) );
   assert ( b1 . newCompany ( "ACME", "Kolejni", "666/666/666" ) );
 
-  b1.print();
-  /*
+
+
+
   assert ( b1 . invoice ( "666/666", 2000 ) );
   assert ( b1 . medianInvoice () == 2000 );
   assert ( b1 . invoice ( "666/666/666", 3000 ) );
   assert ( b1 . medianInvoice () == 3000 );
+
   assert ( b1 . invoice ( "123456", 4000 ) );
   assert ( b1 . medianInvoice () == 3000 );
   assert ( b1 . invoice ( "aCmE", "Kolejni", 5000 ) );
   assert ( b1 . medianInvoice () == 4000 );
   assert ( b1 . audit ( "ACME", "Kolejni", sumIncome ) && sumIncome == 8000 );
   assert ( b1 . audit ( "123456", sumIncome ) && sumIncome == 4000 );
+
   assert ( b1 . firstCompany ( name, addr ) && name == "ACME" && addr == "Kolejni" );
   assert ( b1 . nextCompany ( name, addr ) && name == "ACME" && addr == "Thakurova" );
   assert ( b1 . nextCompany ( name, addr ) && name == "Dummy" && addr == "Thakurova" );
   assert ( ! b1 . nextCompany ( name, addr ) );
+  b1.print();
   assert ( b1 . cancelCompany ( "ACME", "KoLeJnI" ) );
   assert ( b1 . medianInvoice () == 4000 );
+
   assert ( b1 . cancelCompany ( "666/666" ) );
   assert ( b1 . medianInvoice () == 4000 );
   assert ( b1 . invoice ( "123456", 100 ) );
   assert ( b1 . medianInvoice () == 3000 );
+
   assert ( b1 . invoice ( "123456", 300 ) );
   assert ( b1 . medianInvoice () == 3000 );
   assert ( b1 . invoice ( "123456", 200 ) );
   assert ( b1 . medianInvoice () == 2000 );
   assert ( b1 . invoice ( "123456", 230 ) );
-  assert ( b1 . medianInvoice () == 2000 );
+  assert ( b1 . medianInvoice () == 2000 );/*
   assert ( b1 . invoice ( "123456", 830 ) );
   assert ( b1 . medianInvoice () == 830 );
   assert ( b1 . invoice ( "123456", 1830 ) );
@@ -193,11 +351,12 @@ int               main           ( void )
   assert ( b1 . medianInvoice () == 2000 );
   assert ( b1 . invoice ( "123456", 3200 ) );
   assert ( b1 . medianInvoice () == 2000 );
-  assert ( b1 . firstCompany ( name, addr ) && name == "Dummy" && addr == "Thakurova" );
+ // assert ( b1 . firstCompany ( name, addr ) && name == "Dummy" && addr == "Thakurova" );
   assert ( ! b1 . nextCompany ( name, addr ) );
   assert ( b1 . cancelCompany ( "123456" ) );
-  assert ( ! b1 . firstCompany ( name, addr ) );
+  //assert ( ! b1 . firstCompany ( name, addr ) );
 
+*/
     printf("\n");
   CVATRegister b2;
   assert ( b2 . newCompany ( "ACME", "Kolejni", "abcdef" ) );
@@ -228,7 +387,7 @@ int               main           ( void )
   assert ( b2 . newCompany ( "ACME", "Kolejni", "abcdef" ) );
   assert ( b2 . cancelCompany ( "ACME", "Kolejni" ) );
   assert ( ! b2 . cancelCompany ( "ACME", "Kolejni" ) );
-*/
+
   return EXIT_SUCCESS;
 }
 //#endif /* __PROGTEST__ */
